48ee4da62a93b7d6c6fd94f3600ff69198a8e13d
Pradeep Ahuja <pradeep.ahuja@itemgroup.com.au>
2022-11-13 13:41:01 +1100

    changes

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core'
 
 import { Item } from './item'
 import { ItemService } from './item.service'
+import * as fs from '@nativescript/core/file-system';
+import { File, Folder, isAndroid, isIOS, knownFolders, path } from '@nativescript/core';
+var Sqlite = require("nativescript-sqlite");
 
 @Component({
   selector: 'ns-items',
@@ -9,10 +12,17 @@ import { ItemService } from './item.service'
 })
 export class ItemsComponent implements OnInit {
   items: Array<Item>
-
+  imageDatabaseName="T2T_Images.db";
   constructor(private itemService: ItemService) {}
 
   ngOnInit(): void {
+    let imageDBPath = fs.knownFolders.currentApp().path;
+    imageDBPath =path.join(imageDBPath, "/assets/appimages");  
+    imageDBPath = path.join(imageDBPath, this.imageDatabaseName);  
+    console.log("aaaaa12",Sqlite.exists(this.imageDatabaseName));
+    console.log("cccc",Sqlite.exists(imageDBPath));
+    console.log(imageDBPath);
+    const folder = Folder.fromPath(imageDBPath);
     this.items = this.itemService.getItems()
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('@nativescript/webpack');
 
 module.exports = (env) => {
   webpack.init(env);
-
+  webpack.Utils.addCopyRule('assets/**')
   // Learn how to customize:
   // https://docs.nativescript.org/webpack
 
