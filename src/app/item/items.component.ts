23832852334cffd75bf9c498c16494cea68e8a87
Pradeep Ahuja <pradeep.ahuja@itemgroup.com.au>
2022-11-13 14:24:11 +1100

    db move

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -17,36 +17,44 @@ export class ItemsComponent implements OnInit {
   currentImagesDatabase: any=null;
   ngOnInit(): void {
     let imageDBPath = fs.knownFolders.currentApp().path;
-    imageDBPath =path.join(imageDBPath, "/assets/appimages");  
-    imageDBPath = path.join(imageDBPath, this.imageDatabaseName);  
+    //imageDBPath =path.join(imageDBPath, "/assets/appimages");  
+    //imageDBPath = path.join(imageDBPath, this.imageDatabaseName);  
     console.log("aaaaa12",Sqlite.exists(this.imageDatabaseName));
-    console.log("cccc",Sqlite.exists(imageDBPath));
+    console.log("ddddd",Sqlite.exists(imageDBPath));
     console.log(imageDBPath);
-    if (!Sqlite.exists(this.imageDatabaseName)) {
+    let localPath=this.imageDatabaseName + "1112";
+    console.log("eeeee",Sqlite.exists(localPath));
+    if (!Sqlite.exists(localPath)) {
+      Sqlite.copyDatabase(this.imageDatabaseName);
+      /*
       if(isIOS)
       {
         //const fileManager = Sqlite.iosProperty(NSFileManager, NSFileManager.defaultManager);
 
-        let destination = path.join(fs.knownFolders.documents().path ,this.imageDatabaseName);
+        let destination = path.join(fs.knownFolders.documents().path ,localPath);
         NSFileManager.defaultManager.copyItemAtPathToPathError(imageDBPath, destination);
+        console.log("qqqqqqqqqqqqqqqqqqqqqq");
       }
       else
       {
         Sqlite.copyDatabase(imageDBPath);
       }
+      */
       
     }
-    (new Sqlite(this.imageDatabaseName)).then(db => {
+    console.log("eeeeeeeeeeeeeeeeeeeeeeeeeee");
+    console.log("ffffffffffffffff",Sqlite.exists(localPath));
+    (new Sqlite(localPath)).then(db => {
     
       this.currentImagesDatabase = db;
       this.currentImagesDatabase.resultType(Sqlite.RESULTSASOBJECT);
-      let sql=`SELECT uniqueID,imageURL,imageType FROM ImageGallery order by imageType,imageURL limit 1`;
+      let sql=`SELECT uniqueID,imageURL,imageType FROM ImageGallery where downloaded=0 order by imageType,imageURL limit 1`;
       let thisClass=this;
       this.currentImagesDatabase.all(
         sql,
         [],
           function(err, dbResponse) {
-            console.log(dbResponse);
+            console.log("dbResponsedbResponse",dbResponse);
             thisClass.markFileAsDownloaded([dbResponse[0].uniqueID],false);
           });
     },
@@ -79,10 +87,20 @@ export class ItemsComponent implements OnInit {
       imageType:"AA",
       grandTotalOfImagesToDownload:100});
    */
+  
   this.currentImagesDatabase.execSQL("update ImageGallery set downloaded=1 where uniqueID in (" + inCommand + ")",uniqueIDs).then(id => {
     console.log("files marked22223", uniqueIDs.length,startDownload,uniqueIDs[0]);
     
-  
+    let sql="SELECT * FROM ImageGallery where uniqueID='" + uniqueIDs[0] + "'";
+    
+    thisClass.currentImagesDatabase.all(
+      sql,
+      [],
+        function(err, dbResponse) {
+          console.log("aaaaaaaaaaaaaaaaa");
+          console.log(dbResponse);
+          
+        });
   },
   error => {
     
